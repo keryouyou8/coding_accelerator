c2dc866801a5958250ebca82236625e41cd16aa6
keryouyou8 <y.kerboub@hotmail.fr>
2024-11-22 21:23:27 +0100

    Epreuve de la Terre

--- terre00.py
+++ terre00.py
@@ -1,10 +1,11 @@
+#Version 1
 first_letter = "a"
 last_letter = "z"
 
 for letter in range(ord(first_letter), ord(last_letter) + 1):  # 97 = 'a', 123 = 'z' + 1
     print(chr(letter), end="")
     
-    
+#Version 2    
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 for lettre in alphabet:
