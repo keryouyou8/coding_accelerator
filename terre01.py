383ec987c5cb7a8235b2f833f95e5b67d01ccc34
keryouyou8 <y.kerboub@hotmail.fr>
2024-11-22 21:10:11 +0100

    Epreuve de la Terre

--- terre01.py
+++ terre01.py
@@ -1,2 +1,14 @@
-file_name = __file__.split("\\")[-1]
+# Version 1
+import sys  
+
+path = sys.argv[0]
+file_name = path.split("/")[-1]
+
 print(file_name)
+
+
+
+# Version 2
+#file_name = __file__.split("\\")[-1]
+
+# print(file_name)
---
+++ terre02.py
@@ -0,0 +1,5 @@
+import sys
+
+# Parcourir tous les arguments sauf le premier (le nom du fichier)
+for argument in sys.argv[1:]:
+    print(argument)
