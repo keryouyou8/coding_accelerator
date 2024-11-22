4ee6d786214e9171a1307b726eea46028ed290d4
keryouyou8 <y.kerboub@hotmail.fr>
2024-11-22 21:20:24 +0100

    Epreuve de la Terre

--- terre00.py
+++ terre00.py
@@ -1,12 +1,11 @@
+first_letter = "a"
+last_letter = "z"
+
+for letter in range(ord(first_letter), ord(last_letter) + 1):  # 97 = 'a', 123 = 'z' + 1
+    print(chr(letter), end="")
+    
+    
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 for lettre in alphabet:
     print(lettre, end="")
-
-
-# Version 2 en utilisant le ASCII
-#first_letter = "a"
-#last_letter = "z"
-
-#for letter in range(ord(first_letter), ord(last_letter) + 1):  # 97 = 'a', 123 = 'z' + 1
-#    print(chr(letter), end="")
