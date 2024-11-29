9692aadd6faca3b1d93e71fa544ebbc45efada41
keryouyou8 <y.kerboub@hotmail.fr>
2024-11-29 02:01:08 +0100

    Epreuve Terre

--- terre01.py
+++ terre01.py
@@ -1,12 +1,41 @@
+# Version 1
 import sys
 
 path = sys.argv[0]
-file_name = path.split("/")[-1]
+file_name = ""
+
+for char in path:
+    if char == '/' : 
+        file_name = ""
+    else:
+        file_name += char 
 
 print(file_name)
 
 
+
 # Version 2
-#file_name = __file__.split("\\")[-1]
+# file_name = ""
+
+# for char in path[::-1]:  
+#     if char == '/':  
+#     file_name = char + file_name  
+
+# print(file_name)
+
+
+
+# Version 3
+# import sys
+
+# path = sys.argv[0]
+# file_name = path.split("/")[-1]
+
+# print(file_name)
+
+
+
+# Version 4
+# file_name = __file__.split("\\")[-1]
 
 # print(file_name)
