30bfdb561cc10ff608ae55d872dbf306975cf60b
keryouyou8 <y.kerboub@hotmail.fr>
2024-12-19 01:31:43 +0100

    Epreuve Terre

--- terre10.py
+++ terre10.py
@@ -10,21 +10,21 @@ if arg_count != 2:
     print("erreur : Seul un argument est requis.")
     sys.exit()
 
-number = sys.argv[1]
+input_number = sys.argv[1]
 
-if not number.lstrip("-").isdigit() or int(number) <= 1:
-    print("erreur : L'argument doit être un nombre entier positif et supérieur à 1.")
+if not input_number.lstrip("-").isdigit() or int(input_number) <= 1:
+    print("erreur : Entrez un entier positif supérieur à 1.")
     sys.exit()
 
-number = int(number)
+input_number = int(input_number)
 
 is_prime = True
 # print("racine carré: " , int(number ** 0.5))
 # print()
 
-for i in range(2, int(number ** 0.5) + 1):
+for i in range(2, int(input_number ** 0.5) + 1):
     # print("i : " , i)
-    if number % i == 0:
+    if input_number % i == 0:
         # print("le reste de number % i == 0 : ", number % i == 0)
         # print()
         
@@ -38,6 +38,6 @@ for i in range(2, int(number ** 0.5) + 1):
 # print()
 
 if is_prime:
-    print(f"Oui, {number} est un nombre premier.")
+    print(f"Oui, {input_number} est un nombre premier.")
 else:
-    print(f"Non, {number} n’est pas un nombre premier.")
+    print(f"Non, {input_number} n’est pas un nombre premier.")
